516e10be937773750e5043cba9be4d579cf84026
Chris Ren <ren.chris@outlook.com>
2023-10-19 16:01:32 -0700

    feat: client view

---
+++ src/app/api/replicache-pull/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from 'next/server'
+
+// eslint-disable-next-line import/prefer-default-export
+export async function GET() {
+  try {
+    const response = NextResponse.json({
+      // We will discuss these two fields in later steps.
+      lastMutationIDChanges: {},
+      cookie: 42,
+      patch: [
+        { op: 'clear' },
+        {
+          op: 'put',
+          key: 'message/qpdgkvpb9ao',
+          value: {
+            from: 'Jane',
+            content: "Hey, what's for lunch?",
+            order: 1,
+          },
+        },
+        {
+          op: 'put',
+          key: 'message/5ahljadc408',
+          value: {
+            from: 'Fred',
+            content: 'tacos?',
+            order: 2,
+          },
+        },
+      ],
+    })
+    return response
+  } catch (error: unknown) {
+    console.error((error as Error).message)
+    return new Response(JSON.stringify({ message: (error as Error).message }), { status: 500 })
+  }
+}
