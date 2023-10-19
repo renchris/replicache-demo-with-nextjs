4913cfedfb9a1a3b42df0e2f7c88d7c0615e7692
Chris Ren <ren.chris@outlook.com>
2023-10-18 18:42:58 -0700

    feat: use own license key

--- replicache/constructor.ts
+++ replicache/constructor.ts
@@ -1,10 +1,12 @@
+'use client'
+
 import { Replicache } from 'replicache'
 import TUTORIAL_LICENSE_KEY from '@replicache/license'
 
 const initializeReplicache = () => {
   const rep = new Replicache({
     name: 'user42',
-    licenseKey: TUTORIAL_LICENSE_KEY,
+    licenseKey: process.env.LICENSE_KEY ?? TUTORIAL_LICENSE_KEY,
   })
 
   const elm = document.querySelector('#idbName') as HTMLDivElement | null
