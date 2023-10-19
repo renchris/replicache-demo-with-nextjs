f1d93c4bf98c07e252fb536e7d1143fce088c0bf
Chris Ren <ren.chris@outlook.com>
2023-10-18 22:27:49 -0700

    refactor: replicache constructor returns object

--- replicache/constructor.ts
+++ replicache/constructor.ts
@@ -3,16 +3,12 @@
 import { Replicache } from 'replicache'
 import TUTORIAL_LICENSE_KEY from '@replicache/license'
 
-const initializeReplicache = () => {
+const getReplicache = (): Replicache => {
   const rep = new Replicache({
     name: 'user42',
     licenseKey: process.env.LICENSE_KEY ?? TUTORIAL_LICENSE_KEY,
   })
-
-  const elm = document.querySelector('#idbName') as HTMLDivElement | null
-  if (elm) {
-    elm.textContent = rep.idbName
-  }
+  return rep
 }
 
-export default initializeReplicache
+export default getReplicache
--- src/components/ReplicacheIDBName.tsx
+++ src/components/ReplicacheIDBName.tsx
@@ -1,15 +1,19 @@
 'use client'
 
-import initializeReplicache from '@replicache/constructor'
-import { useEffect } from 'react'
+import getReplicache from '@replicache/constructor'
+import { useEffect, useState } from 'react'
 
 const ReplicacheIDBName = () => {
+  const [idbName, setIdbName] = useState('')
   useEffect(() => {
-    initializeReplicache()
+    const rep = getReplicache()
+    setIdbName(rep.idbName)
   }, [])
 
   return (
-    <code id="idbName" />
+    <code>
+      {idbName}
+    </code>
   )
 }
 export default ReplicacheIDBName
