3a0ccccded855b3dc045e0e01ae4a2188a35a1f2
Chris Ren <ren.chris@outlook.com>
2023-11-07 20:08:32 -0800

    feat(row versioning): initialize database

--- db/initializeDatabase.ts
+++ db/initializeDatabase.ts
@@ -1,20 +1,22 @@
-import { replicacheServer } from '../drizzle/schema'
-import db, { serverID } from './index'
+import { replicacheMeta } from '../drizzle/schema'
+import db from './index'
+
+// consider default values https://orm.drizzle.team/docs/indexes-constraints#default
 
 const initializeDatabase = () => {
   const rows = db
     .select()
-    .from(replicacheServer)
+    .from(replicacheMeta)
     .prepare()
     .all()
 
   if (rows.length === 0) {
     // Insert a row to set the global database version in the replicacheServer table.
     console.log('initializing database...')
-    db.insert(replicacheServer)
+    db.insert(replicacheMeta)
       .values({
-        id: serverID,
-        version: 1,
+        key: 'schemaVersion',
+        value: '1',
       }).run()
   } else {
     console.log('database already initialized.')
