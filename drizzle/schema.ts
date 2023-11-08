cf5c12024122a27998c535658388f6f7cce6b077
Chris Ren <ren.chris@outlook.com>
2023-11-08 15:41:36 -0800

    refactor: camel case row names

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -14,10 +14,10 @@ export type InsertReplicacheMeta = InferInsertModel<typeof replicacheMeta>
 // Stores last mutationID processed for each Replicache client.
 export const replicacheClient = sqliteTable('replicache_client', {
   id: text('id').primaryKey().notNull(),
-  client_group_id: text('client_group_id').notNull(),
-  last_mutation_id: integer('last_mutation_id').notNull(),
-  version: integer('version').notNull(),
-  last_modified: integer('last_modified', { mode: 'timestamp_ms' }).notNull(),
+  clientGroupID: text('client_group_id').notNull(),
+  lastMutationID: integer('last_mutation_id').notNull(),
+  clientVersion: integer('version').notNull(),
+  lastModified: integer('last_modified', { mode: 'timestamp_ms' }).notNull(),
 })
 
 export type ReplicacheClient = InferSelectModel<typeof replicacheClient>
@@ -26,9 +26,9 @@ export type InsertReplicacheClient = InferInsertModel<typeof replicacheClient>
 // cvrversion is null until first pull initializes it.
 export const replicacheClientGroup = sqliteTable('replicache_client_group', {
   id: text('id').primaryKey().notNull(),
-  cvr_version: integer('cvr_version'),
-  client_group_version: integer('client_group_version').notNull(),
-  last_modified: integer('last_modified', { mode: 'timestamp_ms' }).notNull(),
+  cvrVersion: integer('cvr_version'),
+  clientGroupVersion: integer('client_group_version').notNull(),
+  lastModified: integer('last_modified', { mode: 'timestamp_ms' }).notNull(),
 })
 
 export type ReplicacheClientGroup = InferSelectModel<typeof replicacheClientGroup>
@@ -38,10 +38,10 @@ export type InsertReplicacheClientGroup = InferInsertModel<typeof replicacheClie
 
 export const list = sqliteTable('list', {
   id: text('id').primaryKey().notNull(),
-  owner_id: text('owner_id').notNull(),
+  ownerID: text('owner_id').notNull(),
   name: text('name').notNull(),
-  row_version: integer('row_version').notNull(),
-  last_modified: integer('last_modified', { mode: 'timestamp_ms' }).notNull(),
+  rowVersion: integer('row_version').notNull(),
+  lastModified: integer('last_modified', { mode: 'timestamp_ms' }).notNull(),
 })
 
 export type List = InferSelectModel<typeof list>
@@ -49,10 +49,10 @@ export type InsertList = InferInsertModel<typeof list>
 
 export const share = sqliteTable('share', {
   id: text('id').primaryKey().notNull(),
-  list_id: text('list_id').notNull(),
-  user_id: text('user_id').notNull(),
-  row_version: integer('row_version').notNull(),
-  last_modified: integer('last_modified', { mode: 'timestamp_ms' }).notNull(),
+  listID: text('list_id').notNull(),
+  userID: text('user_id').notNull(),
+  rowVersion: integer('row_version').notNull(),
+  lastModified: integer('last_modified', { mode: 'timestamp_ms' }).notNull(),
 })
 
 export type Share = InferSelectModel<typeof share>
@@ -60,12 +60,12 @@ export type InsertShare = InferInsertModel<typeof share>
 
 export const item = sqliteTable('item', {
   id: text('id').primaryKey().notNull(),
-  list_id: text('list_id').notNull(),
+  listID: text('list_id').notNull(),
   title: text('title').primaryKey().notNull(),
   complete: integer('complete', { mode: 'boolean' }).notNull(),
   ord: integer('ord').notNull(),
-  row_version: integer('row_version').notNull(),
-  last_modified: integer('last_modified', { mode: 'timestamp_ms' }).notNull(),
+  rowVersion: integer('row_version').notNull(),
+  lastModified: integer('last_modified', { mode: 'timestamp_ms' }).notNull(),
 })
 
 export type Item = InferSelectModel<typeof item>
