9960176c74a67fcea89817093f06e80993c55126
Chris Ren <ren.chris@outlook.com>
2023-11-13 20:02:47 -0800

    feat(row versioning): dynamic pull

--- src/app/api/replicache-pull/route.ts
+++ src/app/api/replicache-pull/route.ts
@@ -1,17 +1,15 @@
 import { NextResponse } from 'next/server'
 import type { PullRequestV1 } from 'replicache'
-import db from 'db'
 import processPull from '@app/pullActions'
 
 // eslint-disable-next-line import/prefer-default-export
 export async function POST(request: Request) {
   const pull: PullRequestV1 = await request.json()
   console.log('Processing pull', JSON.stringify(pull))
-  const { clientGroupID } = pull
-  const fromVersion = Number(pull.cookie) || 0
+
   const t0 = Date.now()
   try {
-    const body = await db.transaction(() => processPull(fromVersion, clientGroupID))
+    const body = processPull(pull)
     const response = NextResponse.json(body)
     return response
   } catch (error: unknown) {
--- src/app/pullActions.ts
+++ src/app/pullActions.ts
@@ -1,109 +1,251 @@
 'use server'
 
-import { eq, and, gt } from 'drizzle-orm'
 import {
-  Message, message, replicacheClient, replicacheServer,
+  eq, and, gt,
+} from 'drizzle-orm'
+import type {
+  PatchOperation, PullRequestV1, PullResponseOKV1, PullResponseV1,
+} from 'replicache'
+import {
+  replicacheClient,
 } from 'drizzle/schema'
-import db, { serverID } from 'db'
-import { PatchOperation, PullResponseV1 } from 'replicache'
+import db from 'db'
+import type {
+  Cookie, ClientRecord, ClientViewRecord, List, SearchResult, Share, Todo,
+} from '@replicache/types'
+import { getClientGroupForUpdate, putClientGroup } from './sharedActions'
+import {
+  getTodos, getShares, getLists, getPutsSince, getDelsSince, searchLists, searchTodosAndShares,
+} from './appActions'
 
-async function getCurrentVersion(): Promise<number> {
-  const currentVersionStatementQuery = db
-    .select({ version: replicacheServer.version })
-    .from(replicacheServer)
-    .where(eq(replicacheServer.id, serverID))
-    .prepare()
+// cvrKey -> ClientViewRecord
+const cvrCache = new Map<string, ClientViewRecord>()
 
-  const currentVersionStatement = currentVersionStatementQuery.get()
-  return currentVersionStatement?.version || 0
+function makeCVRKey(clientGroupID: string, order: number) {
+  return `${clientGroupID}/${order}`
 }
 
-async function getLastMutationIDChanges(
+function getBaseCVR(
   clientGroupID: string,
-  fromVersion: number,
-): Promise<{ [k: string]: number }> {
-  const rowsStatementQuery = db
-    .select({ id: replicacheClient.id, last_mutation_id: replicacheClient.last_mutation_id })
+  cookie: Cookie,
+) {
+  let previousCVR
+
+  if (typeof cookie === 'object' && cookie !== null && typeof cookie.order === 'number') {
+    previousCVR = cvrCache.get(makeCVRKey(clientGroupID, cookie.order))
+  }
+
+  const baseCVR = previousCVR ?? {
+    list: new Map<string, number>(),
+    todo: new Map<string, number>(),
+    share: new Map<string, number>(),
+    clientVersion: 0,
+  }
+
+  console.log({ previousCVR, baseCVR })
+
+  return { previousCVR, baseCVR }
+}
+
+function searchClients(clientGroupID: string, sinceClientVersion: number) {
+  const clientRowStatementQuery = db
+    .select({
+      id: replicacheClient.id,
+      lastMutationID: replicacheClient.lastMutationID,
+      clientVersion: replicacheClient.clientVersion,
+    })
     .from(replicacheClient)
     .where(and(
-      eq(replicacheClient.client_group_id, clientGroupID),
-      gt(replicacheClient.version, fromVersion),
+      eq(replicacheClient.clientGroupID, clientGroupID),
+      gt(replicacheClient.clientVersion, sinceClientVersion),
     ))
     .prepare()
 
-  const rows = rowsStatementQuery
-    .all()
-    .filter((row) => row.last_mutation_id !== null)
+  const clientRows = clientRowStatementQuery.all()
 
-  return Object.fromEntries(rows.map((row) => [row.id, row.last_mutation_id as number]))
-}
-
-async function getChangedDomainObjects(
-  fromVersion: number,
-): Promise<Message[]> {
-  const changed = db
-    .select()
-    .from(message)
-    .where(gt(message.version, fromVersion))
-  return changed
-}
-
-async function getPatch(
-  changedDomainObjects: Message[],
-  fromVersion: number,
-): Promise<PatchOperation[]> {
-  const patch: PatchOperation[] = changedDomainObjects.map((row: Message) => {
-    const {
-      id, sender, content, ord, version: rowVersion, deleted,
-    } = row
-
-    if (deleted) {
-      if (rowVersion && rowVersion > fromVersion) {
-        return {
-          op: 'del',
-          key: `message/${id}`,
-        }
-      }
+  const clients = clientRows.map((row) => {
+    const client: ClientRecord = {
+      id: row.id,
+      clientGroupID,
+      lastMutationID: row.lastMutationID,
+      clientVersion: row.clientVersion,
     }
-    return {
-      op: 'put',
-      key: `message/${id}`,
-      value: {
-        from: sender,
-        content,
-        order: ord,
-      },
-    }
+    return client
   })
-  return patch
+
+  return clients
 }
 
-async function processPull(
-  fromVersion: number,
+function fromSearchResult(result: SearchResult[]): Map<string, number> {
+  const data = new Map<string, number>()
+  result.forEach((row) => data.set(row.id, row.rowVersion))
+  return data
+}
+
+function pullForChanges(
   clientGroupID: string,
-): Promise<PullResponseV1> {
-  // Get current version.
-  const currentVersion = await getCurrentVersion()
-  if (fromVersion > currentVersion) {
-    throw new Error(
-      `fromVersion ${fromVersion} is from the future - aborting. This can happen in development if the server restarts. In that case, clear appliation data in browser and refresh.`,
+  baseCVR: ClientViewRecord,
+  userID: string,
+  cookie: Cookie,
+): {
+    nextCVRVersion: number;
+    nextCVR: ClientViewRecord;
+    clientChanges: ClientRecord[];
+    lists: List[];
+    shares: Share[];
+    todos: Todo[];
+  } {
+  const baseClientGroupRecord = getClientGroupForUpdate(clientGroupID)
+  const clientChanges = searchClients(clientGroupID, baseCVR.clientVersion)
+  const listMeta = searchLists(userID)
+
+  const listIDs = listMeta.map((listRow) => listRow.id)
+
+  const { shareMeta, todoMeta } = searchTodosAndShares(listIDs)
+
+  const nextCVR: ClientViewRecord = {
+    list: fromSearchResult(listMeta),
+    todo: fromSearchResult(todoMeta),
+    share: fromSearchResult(shareMeta),
+    clientVersion: baseClientGroupRecord.clientGroupVersion,
+  }
+
+  const listPuts = getPutsSince(nextCVR.list, baseCVR.list)
+  const sharePuts = getPutsSince(nextCVR.share, baseCVR.share)
+  const todoPuts = getPutsSince(nextCVR.todo, baseCVR.todo)
+
+  let previousCVRVersion = baseClientGroupRecord.cvrVersion
+  if (previousCVRVersion === null) {
+    if (typeof cookie === 'object' && cookie !== null && typeof cookie.order === 'number') {
+      previousCVRVersion = cookie.order
+    } else {
+      previousCVRVersion = 0
+    }
+    console.log(
+      `ClientGroup ${clientGroupID} is new, initializing to ${previousCVRVersion}`,
     )
   }
 
-  // Get lmids for requesting client groups.
-  const lastMutationIDChanges = await getLastMutationIDChanges(clientGroupID, fromVersion)
+  const nextClientGroupRecord = {
+    ...baseClientGroupRecord,
+    cvrVersion: previousCVRVersion + 1,
+  }
 
-  // Get changed domain objects since requested version.
-  const changedDomainObjects = await getChangedDomainObjects(fromVersion)
+  console.log({
+    listPuts, sharePuts, todoPuts, nextClientGroupRecord,
+  })
 
-  // Build and return response.
-  const patch = await getPatch(changedDomainObjects, fromVersion)
+  const lists = getLists(listPuts)
+  const shares = getShares(sharePuts)
+  const todos = getTodos(todoPuts)
+  putClientGroup(nextClientGroupRecord)
 
-  const body: PullResponseV1 = {
-    lastMutationIDChanges: lastMutationIDChanges ?? {},
-    cookie: currentVersion,
+  return {
+    nextCVRVersion: nextClientGroupRecord.cvrVersion,
+    nextCVR,
+    clientChanges,
+    lists,
+    shares,
+    todos,
+  }
+}
+
+function getPatch(
+  previousCVR: ClientViewRecord | undefined,
+  listDels: string[],
+  lists: List[],
+  shareDels: string[],
+  shares: Share[],
+  todoDels: string[],
+  todos: Todo[],
+): PatchOperation[] {
+  const patch: PatchOperation[] = []
+
+  if (previousCVR === undefined) {
+    patch.push({ op: 'clear' })
+  }
+
+  listDels.forEach((id) => {
+    patch.push({ op: 'del', key: `list/${id}` })
+  })
+
+  lists.forEach((listItem) => {
+    patch.push({ op: 'put', key: `list/${listItem.id}`, value: listItem })
+  })
+
+  shareDels.forEach((id) => {
+    patch.push({ op: 'del', key: `share/${id}` })
+  })
+
+  shares.forEach((shareItem) => {
+    patch.push({ op: 'put', key: `share/${shareItem.id}`, value: shareItem })
+  })
+
+  todoDels.forEach((id) => {
+    patch.push({ op: 'del', key: `todo/${id}` })
+  })
+
+  todos.forEach((todoItem) => {
+    patch.push({ op: 'put', key: `todo/${todoItem.id}`, value: todoItem })
+  })
+
+  return patch
+}
+
+function processPull(
+  pull: PullRequestV1,
+): PullResponseV1 {
+  const { clientGroupID, cookie } = pull
+  const replicacheCookie = cookie as Cookie
+  const { previousCVR, baseCVR } = getBaseCVR(clientGroupID, replicacheCookie)
+  // get userID from local/cookie storage or somewhere
+  const userID = '0'
+
+  const {
+    nextCVRVersion,
+    nextCVR,
+    clientChanges,
+    lists,
+    shares,
+    todos,
+  } = db.transaction(() => pullForChanges(
+    clientGroupID,
+    baseCVR,
+    userID,
+    replicacheCookie,
+  ))
+
+  const listDels = getDelsSince(nextCVR.list, baseCVR.list)
+  const shareDels = getDelsSince(nextCVR.share, baseCVR.share)
+  const todoDels = getDelsSince(nextCVR.todo, baseCVR.todo)
+
+  console.log({ listDels, shareDels, todoDels })
+
+  const patch = getPatch(
+    previousCVR,
+    listDels,
+    lists,
+    shareDels,
+    shares,
+    todoDels,
+    todos,
+  )
+
+  const responseCookie: Cookie = {
+    clientGroupID,
+    order: nextCVRVersion,
+  }
+
+  const body: PullResponseOKV1 = {
+    cookie: responseCookie,
+    lastMutationIDChanges: Object.fromEntries(
+      clientChanges.map((clientRecord: ClientRecord):
+      [string, number] => [clientRecord.id, clientRecord.lastMutationID]),
+    ),
     patch,
   }
+
+  cvrCache.set(makeCVRKey(responseCookie.clientGroupID, responseCookie.order), nextCVR)
 
   return body
 }
