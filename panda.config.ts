8fd000a3a15e72c71601100eafa51a37c3666613
Chris Ren <ren.chris@outlook.com>
2023-11-23 17:56:39 -0800

    feat: configure park ui recipe

--- panda.config.ts
+++ panda.config.ts
@@ -1,9 +1,12 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from '@pandacss/dev'
+import button from 'recipes/button.recipe'
 
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
+
+  presets: ['@pandacss/preset-base', '@park-ui/panda-preset'],
 
   // Where to look for your css declarations
   include: ['./src/components/**/*.{ts,tsx,js,jsx}', './src/app/**/*.{ts,tsx,js,jsx}'],
@@ -13,10 +16,21 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      recipes: {
+        button,
+      },
+    },
   },
 
   // The output directory for your css system
   outdir: 'styled-system',
+
+  /**
+   * Outputs base JSX component library
+   * https://panda-css.com/docs/concepts/style-props#jsx-patterns
+   * https://panda-css.com/docs/concepts/patterns
+   *  */
+  jsxFramework: 'react',
 
 })
