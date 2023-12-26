09a3af86d7e46b2e8f13911b9db7de124500e1cb
Chris Ren <ren.chris@outlook.com>
2023-12-26 13:52:42 -0500

    feat: checkbox component

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,23 @@
+import { Checkbox as ArkCheckbox } from '@ark-ui/react'
+import { styled } from 'styled-system/jsx'
+import { checkbox } from 'styled-system/recipes'
+import { createStyleContext } from 'src/lib/create-style-context'
+
+const { withProvider, withContext } = createStyleContext(checkbox)
+
+export const CheckboxRoot = withProvider(styled(ArkCheckbox.Root), 'root')
+export const CheckboxControl = withContext(styled(ArkCheckbox.Control), 'control')
+export const CheckboxIndicator = withContext(styled(ArkCheckbox.Indicator), 'indicator')
+export const CheckboxLabel = withContext(styled(ArkCheckbox.Label), 'label')
+
+export const Checkbox = Object.assign(CheckboxRoot, {
+  Root: CheckboxRoot,
+  Control: CheckboxControl,
+  Indicator: CheckboxIndicator,
+  Label: CheckboxLabel,
+})
+
+export type CheckboxProps = typeof CheckboxRoot
+export type CheckboxControlProps = typeof CheckboxControl
+export type CheckboxIndicatorProps = typeof CheckboxIndicator
+export type CheckboxLabelProps = typeof CheckboxLabel
