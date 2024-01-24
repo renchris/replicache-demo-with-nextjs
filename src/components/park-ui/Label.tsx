9a5677a6f3606975e74fa16617248d3f35ca80c3
Chris Ren <ren.chris@outlook.com>
2024-01-24 17:57:34 -0500

    fix: park-ui label import

--- src/components/park-ui/Label.tsx
+++ src/components/park-ui/Label.tsx
@@ -1,7 +1,7 @@
 import { ark } from '@ark-ui/react/factory'
 import type { ComponentProps } from 'react'
 import { styled } from 'styled-system/jsx'
-import { label } from '@styled-system/recipes/label'
+import { formLabel } from '@styled-system/recipes'
 
-export const Label = styled(ark.label, label)
+export const Label = styled(ark.label, formLabel)
 export interface LabelProps extends ComponentProps<typeof Label> {}
