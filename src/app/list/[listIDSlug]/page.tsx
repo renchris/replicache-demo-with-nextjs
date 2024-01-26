c3813bd26460ee203b97c1573bf974b4a2438ea6
Chris Ren <ren.chris@outlook.com>
2024-01-25 22:06:16 -0500

    feat: no selected list page component

--- src/app/list/[listIDSlug]/page.tsx
+++ src/app/list/[listIDSlug]/page.tsx
@@ -5,12 +5,21 @@ import { useReplicacheContext } from 'src/lib/create-replicache-context'
 import { usePathname } from 'next/navigation'
 import ItemInput from '@components/ItemInput'
 import { handleNewItem } from '@app/todoActions'
+import { useSubscribe } from 'replicache-react'
+import { ReadTransaction } from 'replicache'
+import { getList } from '@replicache/mutators'
+import { css } from '@styled-system/css'
 
 const ListPage = () => {
   const { rep } = useReplicacheContext()
   const [itemName, setItemName] = useState('')
   const pathname = usePathname()
   const listID = pathname.split('/').pop() || ''
+  const selectedList = useSubscribe(
+    rep,
+    (tx: ReadTransaction) => getList(tx, listID),
+    { default: undefined, dependencies: [listID] },
+  )
   const handleSubmitItem = async (text: string) => {
     if (text) {
       await handleNewItem(
@@ -21,11 +30,22 @@ const ListPage = () => {
     }
   }
   return (
-    <ItemInput
-      itemName={itemName}
-      setItemName={setItemName}
-      handleSubmitItem={handleSubmitItem}
-    />
+    selectedList
+      ? (
+        <ItemInput
+          itemName={itemName}
+          setItemName={setItemName}
+          handleSubmitItem={handleSubmitItem}
+        />
+      ) : (
+        <div className={css({
+          padding: '32px',
+        })}
+        >
+          List Cannot be Found With Current Permissions
+        </div>
+      )
+
   )
 }
 
